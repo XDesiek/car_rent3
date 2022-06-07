8eec8e2dcc376dab39ac328e3e2818fa889b2436
“Anton <tony.j.kaminski@gmail.com>
2022-06-07 19:26:41 +0200

    commit nwm wsm co

--- gui.py
+++ gui.py
@@ -1,9 +1,53 @@
 import tkinter as tk
 from tkinter import ttk
 from employees import Employee
+from cars import Car
 from base_car_rent3 import Session
-from gui_functions import login_check
-from tkinter.messagebox import showinfo
+from gui_functions import login_check, all_cars
+from tkinter.messagebox import showinfo,showerror
+
+
+def add_user(root,):
+    root.destroy()
+    
+    window = tk.Tk()
+    window.columnconfigure(0, weight=1)
+    window.columnconfigure(1, weight=3)
+
+    # name
+    name_label = ttk.Label(window, text="name:")
+    name_label.grid(column=0, row=0, sticky=tk.W, padx=5, pady=5)
+
+    name_entry = ttk.Entry(window)
+    name_entry.grid(column=1, row=0, sticky=tk.E, padx=5, pady=5)
+    # surname
+    surname_label = ttk.Label(window, text="surname:")
+    surname_label.grid(column=0, row=1, sticky=tk.W, padx=5, pady=5)
+
+    surname_entry = ttk.Entry(window)
+    surname_entry.grid(column=1, row=1, sticky=tk.E, padx=5, pady=5)
+    # username
+    username_label = ttk.Label(window, text="Username:")
+    username_label.grid(column=0, row=2, sticky=tk.W, padx=5, pady=5)
+
+    username_entry = ttk.Entry(window)
+    username_entry.grid(column=1, row=2, sticky=tk.E, padx=5, pady=5)
+    # password
+    password_label = ttk.Label(window, text="password:")
+    password_label.grid(column=0, row=3, sticky=tk.W, padx=5, pady=5)
+
+    password_entry = ttk.Entry(window)
+    password_entry.grid(column=1, row=3, sticky=tk.E, padx=5, pady=5)
+
+    # login button
+    login_button = ttk.Button(window, text="Login",command=lambda:submit())
+    login_button.grid(column=1, row=4, sticky=tk.E, padx=5, pady=5)
+    
+    window.columnconfigure(0, weight=1)
+    window.columnconfigure(1, weight=3)
+
+
+    window.mainloop()
 
 
 
@@ -33,8 +77,13 @@ password_entry = ttk.Entry(root,) # show="*"
 password_entry.grid(column=1, row=1, sticky=tk.E, padx=5, pady=5)
 
 # login button
-login_button = ttk.Button(root, text="Login",command=lambda:login_check(Session,Employee,username_entry,password_entry,root,tk))
+login_button = ttk.Button(root, text="Login",command=lambda:login_check(Session,Employee,username_entry,password_entry,root))
 login_button.grid(column=1, row=3, sticky=tk.E, padx=5, pady=5)
 
 
+add_user_button = ttk.Button(root, text="add user",command=lambda:login_check(Session,Employee,username_entry,password_entry,root,tk))
+add_user_button.grid(column=0, row=3, sticky=tk.E, padx=5, pady=5)
+
 root.mainloop()
+
+all_cars(Session,Car)
--- gui_functions.py
+++ gui_functions.py
@@ -1,13 +1,85 @@
 
-def login_check(Session,Employee,username_entry,password_entry,root,tk):
-    
+
+def login_check(Session,Employee,username_entry,password_entry,root):
+    import tkinter as tk
     session = Session()
     username_checkbox=username_entry.get()
     password_checkbox=password_entry.get()
     user = session.query(Employee).filter(Employee.username == username_checkbox).all()
     password = session.query(Employee).filter(Employee.password ==password_checkbox).all()
+    
+    
+    # print (f"\n\n\n\n\n {user.type()} \n\n\n\n\n")
+
+
     if user == password:# bedzie sie wieszac przy wielu takich samych paswordach
-        tk.messagebox.showinfo(root,message='everything is correct').pack()
+        root.destroy()
+        tk.messagebox.showinfo(title="login box",message='everything is correct')
+    elif user != password:
+        tk.messagebox.showerror(title="login box",message='login is not correct').pack()    
     session.close()
 
+def all_cars(Session,Car):
+    sessioncar = Session()
+    cars = sessioncar.query(Car).all()
+    for car in cars:
+       print('')
+       print (car)
+    print('')
+    sessioncar.close()
 #user ma jakas dziwna classe i nie jest stringiem  //  potem odpalic nowe window z dodawaniem cars i ogarnianiem
+
+
+
+
+
+
+
+
+
+
+
+# def add_user(root,):
+#     import tkinter as tk
+#     from tkinter import ttk
+#     root.destroy()
+    
+#     window = tk.Tk()
+#     window.columnconfigure(0, weight=1)
+#     window.columnconfigure(1, weight=3)
+
+#     # name
+#     name_label = ttk.Label(window, text="name:")
+#     name_label.grid(column=0, row=0, sticky=tk.W, padx=5, pady=5)
+
+#     name_entry = ttk.Entry(window)
+#     name_entry.grid(column=1, row=0, sticky=tk.E, padx=5, pady=5)
+#     # surname
+#     surname_label = ttk.Label(window, text="surname:")
+#     surname_label.grid(column=0, row=1, sticky=tk.W, padx=5, pady=5)
+
+#     surname_entry = ttk.Entry(window)
+#     surname_entry.grid(column=1, row=1, sticky=tk.E, padx=5, pady=5)
+#     # username
+#     username_label = ttk.Label(window, text="Username:")
+#     username_label.grid(column=0, row=2, sticky=tk.W, padx=5, pady=5)
+
+#     username_entry = ttk.Entry(window)
+#     username_entry.grid(column=1, row=2, sticky=tk.E, padx=5, pady=5)
+#     # password
+#     password_label = ttk.Label(window, text="password:")
+#     password_label.grid(column=0, row=3, sticky=tk.W, padx=5, pady=5)
+
+#     password_entry = ttk.Entry(window)
+#     password_entry.grid(column=1, row=3, sticky=tk.E, padx=5, pady=5)
+
+#     # login button
+#     login_button = ttk.Button(window, text="Login",command=lambda:submit())
+#     login_button.grid(column=1, row=4, sticky=tk.E, padx=5, pady=5)
+    
+#     window.columnconfigure(0, weight=1)
+#     window.columnconfigure(1, weight=3)
+
+
+#     window.mainloop()
+
